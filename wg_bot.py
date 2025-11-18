c9e37c7d710073b1a69f78d25215ec26a58fa674
Oleksii Rudzevych <s185435@student.uken.krakow.pl>
2025-11-18 11:30:58 +0100

    Refactor key generation to use NaCl bindings

--- wg_bot.py
+++ wg_bot.py
@@ -1,16 +1,33 @@
-import subprocess
 import telebot
-print("Bot started! Waiting for commands...")
+import base64
+from nacl import bindings
 
 BOT_TOKEN = "8255873011:AAG3-i0NBFgLVyNe6En1SJQrGGzZuJuJpOA"
 bot = telebot.TeleBot(BOT_TOKEN)
 
-@bot.message_handler(commands=["gen"])
-def gen_wg(message):
-    priv = subprocess.check_output(["wg", "genkey"]).decode().strip()
-    pub = subprocess.check_output(["wg", "pubkey"], input=priv.encode()).decode().strip()
 
-    text = f"PrivateKey: `{priv}`\nPublicKey: `{pub}`"
-    bot.send_message(message.chat.id, text, parse_mode="Markdown")
+def wg_generate_keys():
+    # Private key 32 bytes
+    private_key = bindings.crypto_box_keypair()[0]
+    private_key_b64 = base64.b64encode(private_key).decode()
+
+    # Public key = scalar multiplication
+    public_key = bindings.crypto_scalarmult_base(private_key)
+    public_key_b64 = base64.b64encode(public_key).decode()
+
+    return private_key_b64, public_key_b64
+
+
+@bot.message_handler(commands=['gen'])
+def gen_handler(message):
+    priv, pub = wg_generate_keys()
+
+    bot.send_message(
+        message.chat.id,
+        f"PrivateKey: `{priv}`\nPublicKey: `{pub}`",
+        parse_mode="Markdown"
+    )
+
 
 bot.polling()
+
